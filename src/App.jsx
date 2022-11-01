b51c058cd47d8b0280330cd5200bbc029e312b4c
imskanand <the.anand3099@gmail.com>
2022-11-01 18:50:38 +0530

    Finelly working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import {BrowserEvents as Router, Route, Routes} from 'react-router'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 
-function App() {
+const Home = () => <h1>Home</h1>
+const About = () => <h1>About</h1>
+const Settings = () => <h1>Settings</h1>
 
+export default function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' />
-          <Route path='/about' />
-          <Route path='/settings' />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/settings'>Settings</Link></li>
+      </ul>
+
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/settings' element={ <Settings /> } />
+      </Routes>
+    </Router>
   )
 }
-
-export default App
